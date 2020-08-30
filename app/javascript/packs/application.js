ca195a96a9f03a8f554bef35a8170b66d24cb7ef
John Quarto-vonTivadar <jcquarto@gmail.com>
2020-08-29 23:53:05 -0500

    added package.json and others

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,13 @@ require('@rails/activestorage').start();
 require('channels');
 
 // BEGIN comment these 2 files out when trying to install theme
-import 'bootstrap';
-import 'bootstrap-daterangepicker';
+// because they already are part of the app.js in the theme
+//import 'bootstrap';
+//import 'bootstrap-daterangepicker';
 // END
 
 // BEGIN uncomment this file when trying to install theme
-// import 'app';
+import 'app';
 // END
 
 global.$ = jQuery;
